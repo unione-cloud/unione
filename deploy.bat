633065de103df0a53d3b754d8f1a9ea40537c66e
f9tech <f9tech@163.com>
2025-02-23 22:31:00 +0800

    升级打包脚本

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+chcp 65001
+REM 使用 pscp 上传jar包
+pscp -pw %UNIONE_PASSWD% unione-start/target/unione-start-1.0.0-SNAPSHOT.jar root@%UNIONE_HOST%:/opt/unione/portal
+REM 使用 plink 执行build脚本
+plink -pw %UNIONE_PASSWD% root@%UNIONE_HOST% "cd /opt/unione/portal ; sh build"
+REM 程序更新完成
+pause
