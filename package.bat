5143769d521c6e6cd9780228cc730402d97da7b9
f9tech <f9tech@163.com>
2025-02-23 21:20:09 +0800

    升级：项目backage打包脚本，支持：上传jar包并执行docker命令

--- package.bat
+++ package.bat
@@ -1,1 +1,11 @@
+chcp 65001
+REM 编译源码
 mvn clean package -Dspring.boot.skip=false
+
+REM 使用 pscp 上传jar包
+pscp -pw %UNIONE_PASSWD% unione-start/target/unione-start-1.0.0-SNAPSHOT.jar root@%UNIONE_HOST%:/opt/unione/portal
+
+REM 使用 plink 执行build脚本
+plink -pw %UNIONE_PASSWD% root@%UNIONE_HOST% "cd /opt/unione/portal ; sh build"
+
+pause
