0c1cf06ab86af4ae4398710b527f8240e42ddfe9
jeking <jeking217@163.com>
2025-10-17 23:39:16 +0800

    升级：sql 处理能力

--- unione-beetsql/src/main/java/com/unione/cloud/beetsql/ext/TagSqlWhere.java
+++ unione-beetsql/src/main/java/com/unione/cloud/beetsql/ext/TagSqlWhere.java
@@ -10,11 +10,23 @@ public class TagSqlWhere extends WhereTag {
 	public void render() {
 		try {
 			initTrimArgs(args);
-			StringBuilder sb = buildTrimContent();
-			if("WHEREAND".equalsIgnoreCase(sb.toString().replaceAll("\\s", ""))) {
+			String sb = buildTrimContent().toString();
+			
+			String sql = sb.toString().trim().toUpperCase();
+			if(sql.endsWith("AND")) {
+				sql = sql.substring(0, sql.lastIndexOf(" AND")).trim();
+				sb = sb.substring(0, sb.toUpperCase().lastIndexOf(" AND "));
+			}else if (sql.endsWith("OR")) {
+				sql = sql.substring(0, sql.lastIndexOf(" OR")).trim();
+				sb = sb.substring(0, sb.toUpperCase().lastIndexOf(" OR "));
+			}
+
+			String tmp=sql.replaceAll("\\s", "");
+			if("WHERE".equalsIgnoreCase(tmp) ||"WHEREAND".equalsIgnoreCase(tmp) || "WHEREOR".equalsIgnoreCase(tmp)) {
 				return;
 			}
-			this.ctx.byteWriter.writeString(sb.toString());
+
+			this.ctx.byteWriter.writeString(sb);
 		} catch (IOException ie) {
 			ie.printStackTrace();
 		}
