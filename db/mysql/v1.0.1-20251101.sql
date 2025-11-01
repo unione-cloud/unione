fbcc90d21e97a94d8dbc29bcd5196b49cc7f3231
jeking <jeking217@163.com>
2025-11-01 16:09:10 +0800

    升级动态表单模块

---
+++ db/mysql/v1.0.1-20251101.sql
@@ -0,0 +1,31 @@
+-- 升级动态表单模块
+-- 页面定义：增加页面类型 form:表单设计
+ALTER TABLE `unione`.`sys_page_define` 
+MODIFY COLUMN `TYPES` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '页面分类，字典PAGETYPE  code:编码页面，setting：配置页面，design：设计页面，form：表单设计' AFTER `PIC_MIX`;
+ALTER TABLE `unione`.`sys_page_release` 
+MODIFY COLUMN `TYPES` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '页面分类，字典PAGETYPE  code:编码页面，setting：配置页面，design：设计页面，form：表单设计' AFTER `PIC_MIX`;
+ALTER TABLE `unione`.`sys_page_his` 
+MODIFY COLUMN `TYPES` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '页面分类，字典PAGETYPE  code:编码页面，setting：配置页面，design：设计页面，form：表单设计' AFTER `PIC_MIX`;
+
+-- 数据定义：增加数据定义类型字段
+ALTER TABLE `unione`.`sys_data_define` 
+ADD COLUMN `TYPES` varchar(10) NULL COMMENT '数据定义类型 setting：数据配置，form：表单设计' AFTER `VERS`;
+UPDATE sys_data_define SET TYPES='setting' WHERE TYPES IS NULL;
+ALTER TABLE `unione`.`sys_data_define` 
+MODIFY COLUMN `TYPES` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '数据定义类型 setting：数据配置，form：表单设计' AFTER `VERS`;
+
+ALTER TABLE `unione`.`sys_data_define_release` 
+ADD COLUMN `TYPES` varchar(10) NULL COMMENT '数据定义类型 setting：数据配置，form：表单设计' AFTER `VERS`;
+UPDATE sys_data_define_release SET TYPES='setting' WHERE TYPES IS NULL;
+ALTER TABLE `unione`.`sys_data_define_release` 
+MODIFY COLUMN `TYPES` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '数据定义类型 setting：数据配置，form：表单设计' AFTER `VERS`;
+
+ALTER TABLE `unione`.`sys_data_define_his` 
+ADD COLUMN `TYPES` varchar(10) NULL COMMENT '数据定义类型 setting：数据配置，form：表单设计' AFTER `VERS`;
+UPDATE sys_data_define_his SET TYPES='setting' WHERE TYPES IS NULL;
+ALTER TABLE `unione`.`sys_data_define_his` 
+MODIFY COLUMN `TYPES` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '数据定义类型 setting：数据配置，form：表单设计' AFTER `VERS`;
+
+
+
+
