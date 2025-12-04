9480bda05d9fceb7323a5d406d6b48879a9aa9f1
jeking <jeking217@163.com>
2025-12-04 09:17:45 +0800

    完善banner生成内容

--- unione-core/src/main/java/com/unione/cloud/core/generator/BannerGenerator.java
+++ unione-core/src/main/java/com/unione/cloud/core/generator/BannerGenerator.java
@@ -1,21 +1,30 @@
 package com.unione.cloud.core.generator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class BannerGenerator{
+
+    private String serverName;
+
+    private String serverPort;
     
-    // 静态代码块 - 在类加载时立即执行，这是JVM层面最高优先级的执行方式之一
-    // 当JVM首次加载这个类时，静态代码块会被立即执行，优先级高于Spring容器的初始化过程
-    static {
-        // 直接调用generate方法并打印结果
+    private String serverCtx;
+
+    @Autowired
+    public void setEnv(Environment env) {
+        this.serverName = env.getProperty("spring.application.name");
+        this.serverPort = env.getProperty("server.port");
+        this.serverCtx = env.getProperty("server.servlet.context-path","/");
         System.out.println(generate());
     }
 
-    public static String generate() {
+    public String generate() {
         StringBuilder sb = new StringBuilder();
         sb.append("\n");
         sb.append("             _                         _                 _ \n");
@@ -25,8 +34,10 @@ public class BannerGenerator{
         sb.append(" \\__,_|_| |_| |\\___/|_| |_|\\___|  \\___| |\\___/ \\__,_|\\__,_|\n");
         sb.append("===========================================================\n");
         sb.append("\n");
-        sb.append("  ::  UNIONE CLOUD  ::  (v1.0.0)\n");
+        sb.append("  ::  UNIONE CLOUD  ::  (v1.0.1)\n");
+        sb.append(String.format("  ::  SERVER NAME:%s CTX:%s PORT:%s  ::\n", serverName,serverCtx,serverPort));
         sb.append("\n");
+        sb.append("===========================================================\n");
         return sb.toString();
     }
 }
