a7541ab2fbb8a4359bb2bbc06e9fa499c2ffd5fd
jeking <jeking217@163.com>
2026-01-04 15:48:10 +0800

    升级：sql 查询条件处理逻辑

--- unione-beetsql/src/main/java/com/unione/cloud/beetsql/ext/TagSqlWhere.java
+++ unione-beetsql/src/main/java/com/unione/cloud/beetsql/ext/TagSqlWhere.java
@@ -1,12 +1,16 @@
 package com.unione.cloud.beetsql.ext;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.beetl.sql.core.engine.WhereTag;
 
 import cn.hutool.core.util.ObjectUtil;
 
 public class TagSqlWhere extends WhereTag {
+
+	private Pattern actRegix=Pattern.compile("(\\s+AND\\s+AND\\s+)|(\\s+OR\\s+OR\\s+)|(\\s+AND\\s+OR\\s+)|(\\s+OR\\s+AND\\s+)",Pattern.CASE_INSENSITIVE); 
 
 	@Override
 	public void render() {
@@ -32,14 +36,23 @@ public class TagSqlWhere extends WhereTag {
 				if(ObjectUtil.isEmpty(tmp)){
 					return;
 				}
-				this.ctx.byteWriter.writeString(String.format("WHERE %s ", sb.substring(sql.indexOf(" AND ")+5)));
+				this.ctx.byteWriter.writeString(String.format("WHERE %s ", process(sb.substring(sql.indexOf(" AND ")+5))));
 				return;
 			}
 
-			this.ctx.byteWriter.writeString(sb);
+			this.ctx.byteWriter.writeString(process(sb));
 		} catch (IOException ie) {
 			ie.printStackTrace();
 		}
+	}
+
+	private String process(String sql){
+		Matcher actMatcher=actRegix.matcher(sql);
+		while(actMatcher.find()) {
+			actMatcher.group();
+			sql = sql.replaceAll(actMatcher.group(), String.format(" %s ", actMatcher.group().trim().substring(0,3)));
+		}
+		return sql;
 	}
 	
 }
