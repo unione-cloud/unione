49b1400d40a570d9923ae814d2be04034ca2608b
jeking <jeking217@163.com>
2026-01-12 21:01:37 +0800

    ci: 在部署脚本中使用call调用mvn命令
    避免直接执行mvn命令时可能导致的批处理中断问题

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 chcp 65001
 
 REM 编译源码
-mvn clean package -Dspring.boot.skip=false
+call mvn clean package -Dspring.boot.skip=false
 
 REM 使用 pscp 上传jar包
 pscp -pw %UNIONE_PASSWD% unione-starter/target/unione-starter-1.0.*-SNAPSHOT.jar root@%UNIONE_HOST%:/opt/unione/portal
