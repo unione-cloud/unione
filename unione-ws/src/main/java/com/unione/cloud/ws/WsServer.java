8fe117eb3a8b5134dd42722c1a1962fe48f5cd02
jeking <jeking217@163.com>
2026-01-20 13:17:14 +0800

    完善：ws服务启动，关闭逻辑

--- unione-ws/src/main/java/com/unione/cloud/ws/WsServer.java
+++ unione-ws/src/main/java/com/unione/cloud/ws/WsServer.java
@@ -2,11 +2,14 @@ package com.unione.cloud.ws;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
 
 import com.corundumstudio.socketio.SocketIOServer;
@@ -15,6 +18,7 @@ import com.corundumstudio.socketio.listener.DisconnectListener;
 import com.unione.cloud.core.util.SpringCtxUtil;
 import com.unione.cloud.ws.config.WsProperties;
 import com.unione.cloud.ws.handler.WsAbstractHandler;
+import com.unione.cloud.ws.model.WsNodeEntity;
 
 /**
  * WebSocket连接器
@@ -22,13 +26,16 @@ import com.unione.cloud.ws.handler.WsAbstractHandler;
  */
 @Component
 @ConditionalOnProperty(prefix = "unione.ws", name = "enabled", havingValue = "true", matchIfMissing = true)
-public class WsServer implements ApplicationRunner {
-    
+public class WsServer implements ApplicationListener<ApplicationEvent> {
+
     @Autowired
     private WsClientManager wsClientManager;
-    
+
     @Autowired
     private SocketIOServer socketIOServer;
+
+    @Autowired
+    private WsHealthChecker wsHealthChecker;
 
     @Autowired
     private WsProperties wsProperties;
@@ -40,21 +47,22 @@ public class WsServer implements ApplicationRunner {
     public void init() {
         // 注册连接事件监听器
         socketIOServer.addConnectListener(onConnect());
-        
+
         // 注册断开连接事件监听器
         socketIOServer.addDisconnectListener(onDisconnect());
-        
-        SpringCtxUtil.getBeans(WsAbstractHandler.class).values().forEach(handler->{
+
+        SpringCtxUtil.getBeans(WsAbstractHandler.class).values().forEach(handler -> {
             Type type = handler.getClass().getGenericSuperclass();
             ParameterizedType parameterizedType = (ParameterizedType) type;
             Type[] types = parameterizedType.getActualTypeArguments();
-            socketIOServer.addEventListener(handler.getName(),(Class) types[0], handler);
+            socketIOServer.addEventListener(handler.getName(), (Class) types[0], handler);
         });
-        
+
     }
 
     /**
      * 连接事件监听器
+     * 
      * @return ConnectListener
      */
     private ConnectListener onConnect() {
@@ -65,6 +73,7 @@ public class WsServer implements ApplicationRunner {
 
     /**
      * 断开连接事件监听器
+     * 
      * @return DisconnectListener
      */
     private DisconnectListener onDisconnect() {
@@ -84,33 +93,42 @@ public class WsServer implements ApplicationRunner {
      * 停止WebSocket服务器
      */
     public void stop() {
+        wsHealthChecker.delNode(wsProperties.getNodeId());
         socketIOServer.stop();
     }
 
     /**
      * 获取SocketIOServer实例
+     * 
      * @return SocketIOServer
      */
     public SocketIOServer getServer() {
         return socketIOServer;
     }
 
-    /**
-     * 应用启动时启动WebSocket服务器
-     * @param event ApplicationEvent
-     */
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-    	 this.init();
-         this.start();
-         
-         System.out.println();
-         System.out.println();
-         System.out.println("===========================================================");
-         System.out.println("  ::  WebSocket Server Started  ::  ");
-         System.out.println("  ::  Host: " + wsProperties.getHostname());
-         System.out.println("  ::  Port: " + wsProperties.getPort());
-         System.out.println("  ::  NodeId: " + wsProperties.getNodeId());
-         System.out.println("===========================================================");
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ApplicationStartedEvent) {
+            this.init();
+            this.start();
+            List<WsNodeEntity> nodes = wsHealthChecker.getNodes();
+
+            System.out.println();
+            System.out.println();
+            System.out.println("===========================================================");
+            System.out.println("  ::  WebSocket Server Started  ::  ");
+            System.out.println("  ::  Host: " + wsProperties.getHostname());
+            System.out.println("  ::  Port: " + wsProperties.getPort());
+            System.out.println("  ::  NodeId: " + wsProperties.getNodeId());
+            System.out.println("  ::  Normal Node Count: " + nodes.size());
+            System.out.println("===========================================================");
+        } else if (event instanceof ContextClosedEvent) {
+            this.stop();
+            System.out.println("===========================================================");
+            System.out.println("  ::  WebSocket Server Stopped  ::  ");
+            System.out.println("===========================================================");
+        }
+
     }
+
 }
