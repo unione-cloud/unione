dec611550e5779757c72da15c215e6fd1e55947a
jeking <jeking217@163.com>
2026-04-14 09:19:43 +0800

    完善：组件设置模块

--- db/mysql/v1.0.1-20260413.sql
+++ db/mysql/v1.0.1-20260413.sql
@@ -1,16 +1,17 @@
 -- 升级页面配置，增加组件配置模块，支持用户动态配置组件信息
-drop index normal on SYS_WIDGET_TEMPL;
-drop table if exists SYS_WIDGET_TEMPL;
+drop index normal on SYS_WIDGET_TMPL;
+drop table if exists SYS_WIDGET_TMPL;
 /*==============================================================*/
-/* Table: SYS_WIDGET_TEMPL                                      */
+/* Table: SYS_WIDGET_TMPL                                      */
 /*==============================================================*/
-create table SYS_WIDGET_TEMPL
+create table SYS_WIDGET_TMPL
 (
    ID                   bigint not null,
    TENANT_ID            bigint not null comment '租户ID',
-   PAGE_ID              bigint not null comment '页面ID',
+   OWNER_ID             bigint not null comment '页面定义ID，或数据定义ID',
    WID                  varchar(50) not null comment '组件ID',
    WNAME                varchar(20) not null comment '组件名称',
+   WTITLE               varchar(100) not null comment '模版标题',
    CONFIGS              longtext not null comment '页面设置，json结构{}',
    BIZ_TYPE             int(2) not null comment '业务分类，字典WIDGETTMPLTYPE 9其他',
    DESCS                varchar(200) comment '备注',
@@ -20,11 +21,11 @@ create table SYS_WIDGET_TEMPL
    LAST_UPDATED_BY      bigint not null,
    primary key (ID)
 );
-alter table SYS_WIDGET_TEMPL comment '系统管理：组件模版';
+alter table SYS_WIDGET_TMPL comment '系统管理：组件模版';
 /*==============================================================*/
 /* Index: normal                                                */
 /*==============================================================*/
-create index normal on SYS_WIDGET_TEMPL
+create index normal on SYS_WIDGET_TMPL
 (
    WID,
    WNAME
