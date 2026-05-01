4620ea803ff344e6b057fadfbe2f30165d4df573
jeking <jeking217@163.com>
2026-05-01 16:11:30 +0800

    完善：deploy脚本，支持多portal，gateway构建以及多host构建

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,45 @@
 chcp 65001
 
-REM 编译源码
-call mvn clean package -Dspring.boot.skip=false
+REM 获取脚本参数
+SET HOST=%1
+SET APP=%2
+if "%APP%"=="" (
+ SET APP=portal
+)
+if "%HOST%"=="gateway" (
+ SET APP=gateway
+ SET HOST=CLOUD
+)
+if "%HOST%"=="" (
+ SET HOST=CLOUD
+)
+
+REM 拼接变量
+call set UNIONE_PASSWD=%%UNIONE_%HOST%_PASSWD%%
+call set UNIONE_HOST=%%UNIONE_%HOST%_HOST%%
+
+if "%UNIONE_HOST%"=="" (
+ REM 退出脚本
+ echo "请指定主机名"
+ exit
+)
+
+REM 输出HOST
+echo HOST: %UNIONE_HOST% APP: %APP%
+
+call mvn clean package -Dspring.boot.skip=false 
+
+REM 删除旧的jar包
+plink -pw %UNIONE_PASSWD% root@%UNIONE_HOST% "cd /opt/builder/jars ; rm -f %APP%.jar"
 
 REM 使用 pscp 上传jar包
-pscp -pw %UNIONE_PASSWD% unione-starter/target/unione-starter-1.0.*-SNAPSHOT.jar root@%UNIONE_HOST%:/opt/unione/portal
+if %APP%=="portal" (
+    pscp -pw %UNIONE_PASSWD% unione-starter/target/%APP%.jar root@%UNIONE_HOST%:/opt/builder/jars
+) else (
+    pscp -pw %UNIONE_PASSWD% unione-%APP%/target/%APP%.jar root@%UNIONE_HOST%:/opt/builder/jars
+)
+
 REM 使用 plink 执行build脚本
-plink -pw %UNIONE_PASSWD% root@%UNIONE_HOST% "cd /opt/unione/portal ; sh build"
+plink -pw %UNIONE_PASSWD% root@%UNIONE_HOST% "cd /opt/builder; sh build %APP%"
 REM 程序更新完成
 pause
